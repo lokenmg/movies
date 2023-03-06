d290e960fb4be43299cd49de3bd6e4c243e64901
adr101mg González <adr101mg@hotmail.com>
2023-03-06 13:37:57 -0600

    add mutation

--- peliculas/schema.py
+++ peliculas/schema.py
@@ -14,3 +14,48 @@ class Query(graphene.ObjectType):
 
     def resolve_peliculas(self, info, **kwargs):
         return Pelicula.objects.all()
+    
+class CreatePelicula(graphene.Mutation):
+    id          = graphene.Int()
+    nombre      = graphene.String()
+    estudio     = graphene.String()
+    genero      = graphene.String()
+    duracion    = graphene.Int()
+    recaudacion = graphene.Float()
+    productor   = graphene.String()
+    valoracion  = graphene.Int()
+    servicio    = graphene.String()
+
+    #2
+    class Arguments:
+        nombre      = graphene.String()
+        estudio     = graphene.String()
+        genero      = graphene.String()
+        duracion    = graphene.Int()
+        recaudacion = graphene.Float()
+        productor   = graphene.String()
+        valoracion  = graphene.Int()
+        servicio    = graphene.String()
+
+    #3
+    def mutate(self, info, nombre, estudio, genero, duracion, recaudacion, productor, valoracion, servicio):
+        pelicula = Pelicula(nombre=nombre, estudio=estudio, genero=genero, duracion=duracion, 
+                    recaudacion=recaudacion, productor=productor, valoracion=valoracion, servicio=servicio)
+        pelicula.save()
+
+        return CreatePelicula(
+            id=pelicula.id,
+            nombre=pelicula.nombre,
+            estudio=pelicula.estudio,
+            genero=pelicula.genero,
+            duracion=pelicula.duracion,
+            recaudacion=pelicula.recaudacion,
+            productor=pelicula.productor,
+            valoracion=pelicula.valoracion,
+            servicio=pelicula.servicio
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_peliculas = CreatePelicula.Field()
